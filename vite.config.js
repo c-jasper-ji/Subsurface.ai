efe312c8ea57be1ddb9d6e95ca37381b8287426f
c-jasper-ji <c.jasper.ji@gmail.com>
2026-06-16 16:33:15 +0100

    Deploy app with GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'node:process'
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/Subsurface.ai/' : '/',
   plugins: [react(), tailwindcss()],
 })
